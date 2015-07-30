384e6c219d5c607233f123749640c9caed72ec0a
Brian Volk <bvolk@palantir.com>
2015-07-29 18:41:11 -0700

    server: true

--- config/dev.exs
+++ config/dev.exs
@@ -11,7 +11,8 @@ config :js_test, JsTest.Endpoint,
   debug_errors: true,
   code_reloader: true,
   cache_static_lookup: false,
-  watchers: [node: ["node_modules/brunch/bin/brunch", "watch"]]
+  watchers: [node: ["node_modules/brunch/bin/brunch", "watch"]],
+  server: true
 
 # Watch static and templates for browser reloading.
 config :js_test, JsTest.Endpoint,
